a9850e353dda3f53ff9c482c9ebb966f040274db
Enrique Mera <esy.mera@yavirac.edu.ec>
2023-01-14 21:22:38 -0500

    fdgsdfgdsfgdfg

--- proyecto/backend/src/app.controller.ts
+++ proyecto/backend/src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Datos } from './interfases/datos';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('mostrarDatos')
+  getHello(): number {
+    //return this.appService.getHello();
+    return this.appService.suma();
+  }
+
+  @Post('Ingresar')
+  ingresar(@Body() datos:Datos): string {
+    //return this.appService.getHello();
+    console.log(datos.apellido)
+    return 'hola '+datos.apellido;
   }
 }
--- proyecto/backend/src/app.service.ts
+++ proyecto/backend/src/app.service.ts
@@ -5,4 +5,7 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  suma():number{
+    return 2+2;
+  }
 }

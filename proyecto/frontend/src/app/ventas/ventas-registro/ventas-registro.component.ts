20bc0b1aef28e0b846f59cb5dcf6494bb53104f6
Enrique Mera <esy.mera@yavirac.edu.ec>
2023-01-23 20:47:32 -0500

    prueba

--- proyecto/frontend/src/app/ventas/ventas-registro/ventas-registro.component.ts
+++ proyecto/frontend/src/app/ventas/ventas-registro/ventas-registro.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./ventas-registro.component.css']
 })
 export class VentasRegistroComponent {
-
+  ngOnInit(){
+    console.log('hola')
+  }
 }

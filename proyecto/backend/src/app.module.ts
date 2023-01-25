749613f77792d575c22dc7d0781f09ca4ac1387f
Enrique Mera <esy.mera@yavirac.edu.ec>
2023-01-25 15:55:03 -0500

    module cventas

--- proyecto/backend/src/app.module.ts
+++ proyecto/backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { VentasModule } from './ventas/ventas.module';
 
 @Module({
   imports: [
@@ -15,7 +16,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     entities: [__dirname + '/**/*.entity{.ts,.js}'], 
     synchronize: true 
 
-})],
+}),
+    VentasModule],
   controllers: [AppController],
   providers: [AppService],
 })

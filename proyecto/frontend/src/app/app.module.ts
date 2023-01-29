8b15d86f3e26416faa6b48cc8827d272d3fef9c4
Enrique Mera <esy.mera@yavirac.edu.ec>
2023-01-29 16:15:07 -0500

    fe<sfs<ef<ef

--- proyecto/frontend/src/app/app.module.ts
+++ proyecto/frontend/src/app/app.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
+
 
 
 @NgModule({
@@ -14,10 +12,6 @@ import {MatPaginatorModule} from '@angular/material/paginator';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatCardModule,
-    MatTableModule,
-    MatPaginatorModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]

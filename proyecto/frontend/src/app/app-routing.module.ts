e9b03386bdef4c9daf0933e38ca6c8131d891375
Enrique Mera <esy.mera@yavirac.edu.ec>
2023-01-31 17:23:49 -0500

    fdsgsdfgsdfg

--- proyecto/frontend/src/app/app-routing.module.ts
+++ proyecto/frontend/src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent},
   { path: 'compras', loadChildren: () => import('./compras/compras.module').then(m => m.ComprasModule) },
   { path: 'inventario', loadChildren: () => import('./inventario/inventario.module').then(m => m.InventarioModule) },
   { path: 'ventas', loadChildren: () => import('./ventas/ventas.module').then(m => m.VentasModule) },
+  { path: '**', component: AppComponent},
 ];
 
 
--- proyecto/frontend/src/app/app.module.ts
+++ proyecto/frontend/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,

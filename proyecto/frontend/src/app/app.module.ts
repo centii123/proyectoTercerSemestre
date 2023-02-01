9bcb13aee8130e3df617f7bfd27313649e6b3015
Enrique Mera <esy.mera@yavirac.edu.ec>
2023-01-31 20:56:28 -0500

    gfdsgdfg

--- proyecto/frontend/src/app/app.module.ts
+++ proyecto/frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HttpClientModule } from '@angular/common/http'
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

93f17c23cd8a844de7680b2d4545ebdafb1eee34
¨Leonardo <98435590+LeonardoGBM@users.noreply.github.com>
2023-02-04 17:46:20 -0500

    as

--- proyecto/frontend/src/app/ventas/ventas-registro/ventas-registro.component.ts
+++ proyecto/frontend/src/app/ventas/ventas-registro/ventas-registro.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ventas-registro',
   templateUrl: './ventas-registro.component.html',
   styleUrls: ['./ventas-registro.component.css']
 })
-export class VentasRegistroComponent {
+export class VentasRegistroComponent implements OnInit{
   ngOnInit(){
     console.log('hola')
+  }
+  readonly local = 'localhost:3000/cliente'
+  constructor(private http:HttpClient){}
+  getRegistro(){
+    const data={
+      cedula:String,
+      nombres:String,
+      apellido:String,
+      email:String,
+    }
+    this.http.post(this.local,data).subscribe(e=>{console.log(e)});
   }
 }

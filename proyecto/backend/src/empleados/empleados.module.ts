f1fdfd449840a1e5adcffbbacc34e1f394401867
misterMORTAL <102058753+misterMORTAL@users.noreply.github.com>
2023-02-07 16:48:43 -0500

    listo

--- proyecto/backend/src/empleados/empleados.module.ts
+++ proyecto/backend/src/empleados/empleados.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmpleadosController } from './empleados.controller';
 import { EmpleadosService } from './empleados.service';
+import { empleados } from 'src/dataBase/empleados.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EmpleadosController])],
+  imports: [TypeOrmModule.forFeature([empleados])],
   controllers: [EmpleadosController],
   providers: [EmpleadosService],
 })

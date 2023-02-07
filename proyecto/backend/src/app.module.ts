c86fab2466ccbe08090b7a0d9824041ac61883f3
tz <tzcristhiangt90@gmail.com>
2023-02-07 10:03:48 -0500

    Modelo,servicio empleados

--- proyecto/backend/src/app.module.ts
+++ proyecto/backend/src/app.module.ts
@@ -13,7 +13,7 @@ import { ComprasModule } from './compras/compras.module';
     host: 'localhost',
     port: 5432,
     username: 'postgres',
-    password: '1234',
+    password: '123',
     database: 'proyecto2',
     entities: [__dirname + '/**/*.entity{.ts,.js}'], 
     synchronize: true 

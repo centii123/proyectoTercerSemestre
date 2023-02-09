17dbcc7cdcd0dec617fb29e2cac3e16d0a7aaf13
tz <tzcristhiangt90@gmail.com>
2023-02-09 15:55:20 -0500

    post, update categoria

--- proyecto/frontend/src/app/models/listar-categorias.model.ts
+++ proyecto/frontend/src/app/models/listar-categorias.model.ts
@@ -3,6 +3,6 @@ export interface CategoriasModel {
   nombre_cat: string;
 }
 
-export interface CreateCategoriasModel extends Omit<CategoriasModel, 'id_cat'| 'nombre_cat'> {
-  id_cat: number;
+export interface CreateCategoriasModel extends Omit<CategoriasModel, 'id_cat'> {
+ 
 }
--- proyecto/frontend/src/app/services/categorias.service.ts
+++ proyecto/frontend/src/app/services/categorias.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { CategoriasModel } from '../models/listar-categorias.model';
+import { CategoriasModel, CreateCategoriasModel } from '../models/listar-categorias.model';
+import { CreatelistarModel } from '../models/listar.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,14 @@ export class CategoriasService {
   readonly Api_url: string = 'http://localhost:3000/categorias';
   constructor(private httpClient: HttpClient) { }
 
-  store(categorias: CategoriasModel):Observable<CategoriasModel>{
+  store(categorias: CreateCategoriasModel):Observable<CreateCategoriasModel>{
     const url = `${this.Api_url}`;
+    const response = this.httpClient.post<CreateCategoriasModel>(url, categorias);
+    return response;
+  }
+
+  update(categorias: CategoriasModel,id: number):Observable<CategoriasModel>{
+    const url = `${this.Api_url}/${id}`;
     const response = this.httpClient.post<CategoriasModel>(url, categorias);
     return response;
   }

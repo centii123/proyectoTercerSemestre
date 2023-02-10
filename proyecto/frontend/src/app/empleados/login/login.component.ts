276d80c35cb28578ffcf67ec6d071d6ca771e72e
tz <tzcristhiangt90@gmail.com>
2023-02-09 23:41:38 -0500

    Login

--- proyecto/frontend/src/app/empleados/login/login.component.ts
+++ proyecto/frontend/src/app/empleados/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component,OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CreateEmpleadoModel, EmpleadoModel } from 'src/app/models/empleado.model';
+import { EmpleadosService } from 'src/app/services/empleados.service';
 import { LoginService } from 'src/app/services/login.service';
 
 @Component({
@@ -10,8 +13,10 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     
   }
-  constructor (private loginService: LoginService) {
+  constructor (private loginService: LoginService,private empleadosService: EmpleadosService,private router: Router) {
   }
+  empleados: EmpleadoModel[] = []
+  rol: number = 0;
   logeo = {
     email: '',
     clave: ''
@@ -19,16 +24,36 @@ export class LoginComponent implements OnInit {
 
   login(){
     try {
-      this.logearEmpleado()
+      this.logearEmpleado();
+      this.compareEmpleados();
     } catch (error) {
       console.log(error)
     }
   }
   logearEmpleado() {
-    const response = this.loginService
-      .login(this.logeo)
+    const response = this.empleadosService
+      .getAll()
       .subscribe((response) => {
         console.log(response);
+        this.empleados = response
      });
+  }
+  compareEmpleados(){
+    this.empleados.forEach(empleado => {
+      if (empleado.email === this.logeo.email && empleado.clave === this.logeo.clave) {
+        this.rol = empleado.id_rol.id_rol
+      }
+    });
+  }
+  redirect(){
+    if (this.rol === 1) {
+      this.router.navigate(['/ventas/inicio'])
+    } else if(this.rol === 2) {
+      this.router.navigate(['/'])
+    }else if(this.rol === 3){
+      this.router.navigate(['/'])
+    }else {
+      this.router.navigate(['/login'])
+    }
   }
 }
